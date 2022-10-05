ad4025f4371f55cb9b5d558bc8f0f63e0b9024d3
Christian <chrille_0313@hotmail.com>
2022-10-06 01:37:50 +0200

    Update color theme

--- PathViz/globals.py
+++ PathViz/globals.py
@@ -5,9 +5,10 @@ class Colors:
     BG = (50, 50, 50)
     GRID = BG
     WALL = (0, 0, 0)
-    VISITED = 0x09CDDA
-    PATH = (255, 255, 0)
-    START = END = (0, 200, 0)
+    VISITED = (194, 15, 0)
+    SEARCHING = (105, 193, 0)  # (204, 204, 0)
+    # PATH = (105, 193, 0)
+    START = END = PATH = (37, 139, 174)
 
 
 class Event(Enum):
@@ -21,9 +22,10 @@ class CellColors:
 
 
 class GridColorTheme:
-    def __init__(self, default, start, end, path, visited, wall):
+    def __init__(self, default, start, end, path, visited, searching, wall):
         self.default = CellColors(default, wall)
         self.visited = CellColors(visited, wall)
+        self.searching = CellColors(searching, wall)
         self.path = CellColors(path, wall)
         self.start = CellColors(start, wall)
         self.end = CellColors(end, wall)
@@ -31,4 +33,4 @@ class GridColorTheme:
 
 WIDTH, HEIGHT = 30, 30
 WIN_WIDTH, WIN_HEIGHT = 600, 600
-COLOR_THEME = GridColorTheme(Colors.GRID, Colors.START, Colors.END, Colors.PATH, Colors.VISITED, Colors.WALL)
+COLOR_THEME = GridColorTheme(Colors.GRID, Colors.START, Colors.END, Colors.PATH, Colors.VISITED, Colors.SEARCHING, Colors.WALL)
