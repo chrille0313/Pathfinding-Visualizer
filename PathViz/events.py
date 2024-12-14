d463be5d60bf4a6a47e641e4d94d2f893624e536
Christian <chrille_0313@hotmail.com>
2024-12-14 04:28:38 +0100

    refactor: add types

--- PathViz/events.py
+++ PathViz/events.py
@@ -1,29 +1,37 @@
 from enum import Enum, auto
 
+class Event(Enum):
+    MODEL_UPDATED = auto()
+
 
 class EventManager:
     def __init__(self):
-        self.subscribers = {}
+        self.subscribers: dict[Event, callable] = {}
 
-    def subscribe(self, event, fn):
+    def subscribe(self, event: Event, fn: callable):
         if event not in self.subscribers:
             self.subscribers[event] = []
 
         self.subscribers[event].append(fn)
 
-    def unsubscribe(self, event, fn):
+    def unsubscribe(self, event: Event, fn: callable):
         if event not in self.subscribers:
             return
 
         self.subscribers[event].remove(fn)
 
-    def publish(self, event, *data):
+    def unsubscribe_all(self, event: Event):
+        if event not in self.subscribers:
+            return
+
+        self.subscribers[event] = []
+
+    def clear_subscribers(self):
+        self.subscribers = {}
+
+    def publish(self, event: Event, *data):
         if event not in self.subscribers:
             return
 
         for fn in self.subscribers[event]:
             fn(*data)
-
-
-class Event(Enum):
-    MODEL_UPDATED = auto()
